b7ab59f44b01cd15901791231ee739603b3d8ad9
ph <helluy@unistra.fr>
2021-10-04 16:49:49 +0200

    up

--- src/main.rs
+++ src/main.rs
@@ -68,7 +68,7 @@ fn main() {
             .zip(un.par_iter().skip(1)) // skip the first element: shifted vector
             .for_each(|((u1, u0), v0)| *u1 = *u0 - C * dt / dx * (*v0 - *u0));
 
-        t = t + dt;
+        t += dt;
         unp1[nx] = exact_sol(xc[0], t);
 
         un.par_iter_mut()
@@ -88,7 +88,7 @@ use std::fs::File;
 use std::io::BufWriter;
 use std::io::Write;
 
-fn sauv_sol(t: f64, xc: &Vec<f64>, un: &Vec<f64>, filename: &str) {
+fn sauv_sol(t: f64, xc: &[f64], un: &[f64], filename: &str) {
     let meshfile = File::create(filename).unwrap();
     let mut meshfile = BufWriter::new(meshfile); // create a buffer for faster writes...
 
